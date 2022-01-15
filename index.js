16d9b7154b5120f4a63597bcdc3552b3dac54986
Baktash Sana <baktash11@gmail.com>
2022-01-15 03:05:20 -0500

    sum of all numbers from one up to (including) some number n.

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ function reversString (str){
     return newStr;
 }
 
-
-
 console.log(reversString("baktash sana a software engineer!"));
+
+// calculates the sum of all numbers from 1 up to (and including) some number n.
+
+const addUpTo = (n)=>{
+    let total =0; 
+    for (let i = 0; i < n; i++) {
+       total += i; 
+    }
+    return total; 
+}
+
+console.log(addUpTo(99));
